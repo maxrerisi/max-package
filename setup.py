3e372dc0efb6dc70df3ff7497f63ed0b38b1edd4
Max R <mrerisi26@cgps.org>
2024-10-23 18:12:48 -0400

    v0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 
 setup(
-    name='myPackage',
+    name='max-package',
     version='0.2.2',
     packages=find_packages(),
     description='A simple Python package',
